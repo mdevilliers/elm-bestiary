649885f461d94f18c96350565988ece952d26a9d
Mark deVilliers <markdevilliers@gmail.com>
2016-09-27 12:04:18 +0100

    add elm-format

--- thingful-explorer/src/Main.elm
+++ thingful-explorer/src/Main.elm
@@ -1,61 +1,69 @@
+module Main exposing (..)
 
 import Http
-import Json.Decode exposing ((:=)) 
+import Json.Decode exposing ((:=))
 import JsonApi
 import JsonApi.Decode
 import JsonApi.Resources
 import JsonApi.Documents
 import Task exposing (..)
-
 import Html exposing (Html, button, div, text)
 import Html.App as Html
 import Html.Events exposing (onClick)
 
 
-main = text "hello"
+main =
+    text "hello"
 
-type alias Model = 
-    {
-    s : String
+
+type alias Model =
+    { s : String
     }
 
-type Msg = FetchSucceded String
+
+type Msg
+    = FetchSucceded String
     | FetchFailed Http.Error
 
 
 init : Model
 init =
-    (Model "empty" )
+    (Model "empty")
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
+    case msg of
+        FetchSucceded newUrl ->
+            ( model, Cmd.none )
 
-    FetchSucceded newUrl ->
-      (model, Cmd.none)
+        FetchFailed _ ->
+            ( model, Cmd.none )
 
-    FetchFailed _ ->
-      (model, Cmd.none)
 
 
 --   List Thing
 
-type alias Thing = 
+
+type alias Thing =
     { uid : String
     , title : String
     }
 
+
 thingDecoder : Json.Decode.Decoder Thing
 thingDecoder =
     Json.Decode.object2 Thing
-      ("uid" := Json.Decode.string)
-      ("title" := Json.Decode.string)
+        ("uid" := Json.Decode.string)
+        ("title" := Json.Decode.string)
+
 
 getThingResource : String -> Task Http.Error (JsonApi.Document)
 getThingResource query =
     Http.get JsonApi.Decode.document ("https://api.thingful.net/things?geobound-maxlat=51.52909&geobound-maxlong=-0.064632&geobound-minlat=51.511104&geobound-minlong=-0.093544&limit=2&sort=score")
 
+
 doSearch : JsonApi.Document -> Result String Thing
 doSearch doc =
-      JsonApi.Documents.primaryResource doc
-          `Result.andThen` (JsonApi.Resources.attributes thingDecoder)
+    JsonApi.Documents.primaryResource doc
+        `Result.andThen` (JsonApi.Resources.attributes thingDecoder)
