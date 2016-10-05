7824965daa0af6bd42a13439075dd8c2748a2bcb
Mark deVilliers <markdevilliers@gmail.com>
2016-10-05 13:26:03 +0100

    run elm-format

--- simple-click-game/src/Main.elm
+++ simple-click-game/src/Main.elm
@@ -2,8 +2,6 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.App as Html
-
-
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
@@ -21,32 +19,32 @@ main =
 
 -- MODEL
 
+
 type alias Model =
-    {
-       currentGame : Maybe Game
+    { currentGame : Maybe Game
     }
 
 
 type alias Game =
-    {
-       size : Int
-       ,moves : Int
-       ,board : List Cell
-       ,gameWon : Bool
+    { size : Int
+    , moves : Int
+    , board : List Cell
+    , gameWon : Bool
     }
 
+
 type alias Cell =
-    {
-        selected : Bool
-        , x : Int
-        , y : Int
+    { selected : Bool
+    , x : Int
+    , y : Int
     }
+
 
 initialModel : Model
 initialModel =
-    {
-        currentGame =Nothing
+    { currentGame = Nothing
     }
+
 
 init : ( Model, Cmd Msg )
 init =
@@ -68,97 +66,146 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
         Selected cell ->
             case model.currentGame of
-                Just game ->( { model | currentGame = Just (applySelected game cell )} , Cmd.none)
-                Nothing -> (model, Cmd.none)
-        NewGame -> 
-            ( Model (Just (newBoard 3)), Cmd.none)
+                Just game ->
+                    ( { model | currentGame = Just (applySelected game cell) }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        NewGame ->
+            ( Model (Just (newBoard 3)), Cmd.none )
 
 
-newBoard : Int -> Game 
+newBoard : Int -> Game
 newBoard size =
-    { 
-        size = size
-        ,moves = 0
-        ,board = [ 
-             Cell False 0 0, Cell False 0 1, Cell False 0 2 
-            , Cell False 1 0, Cell True 1 1, Cell False 1 2 
-            , Cell False 2 0, Cell False 2 1, Cell False 2 2 
+    { size = size
+    , moves = 0
+    , board =
+        [ Cell False 0 0
+        , Cell False 0 1
+        , Cell False 0 2
+        , Cell False 1 0
+        , Cell True 1 1
+        , Cell False 1 2
+        , Cell False 2 0
+        , Cell False 2 1
+        , Cell False 2 2
         ]
-        ,gameWon = False
+    , gameWon = False
     }
 
 
 applySelected : Game -> Cell -> Game
 applySelected game selected =
     let
-        (flippers, others) = findFlippers selected game.board
+        ( flippers, others ) =
+            findFlippers selected game.board
+
         --_ = Debug.log "flippers" (toString flippers)
         --_ = Debug.log "others" (toString others)
-        flipped = flipCells flippers
+        flipped =
+            flipCells flippers
+
         --_ = Debug.log "all flipped" ( toString flipped)
-        board = List.append flipped others
+        board =
+            List.append flipped others
+
         --_ = Debug.log "new board" (toString board)
-        board_sorted = List.sortWith cellSorter board
+        board_sorted =
+            List.sortWith cellSorter board
+
         --_ = Debug.log "board sorted" (toString board_sorted)
     in
         { game | board = board_sorted, moves = (game.moves + 1), gameWon = hasWinner (board_sorted) }
+
 
 hasWinner : List Cell -> Bool
 hasWinner cells =
     List.all (\a -> a.selected) cells || List.all (\a -> a.selected == False) cells
 
+
 flipCells : List Cell -> List Cell
 flipCells cells =
     List.map flipCell cells
 
+
 flipCell : Cell -> Cell
 flipCell cell =
     case cell.selected of
-        False -> Cell True cell.x cell.y
-        True -> Cell False cell.x cell.y
+        False ->
+            Cell True cell.x cell.y
 
-findFlippers : Cell -> List Cell -> (List Cell, List Cell) 
+        True ->
+            Cell False cell.x cell.y
+
+
+findFlippers : Cell -> List Cell -> ( List Cell, List Cell )
 findFlippers cell board =
-        List.partition (\a -> (willFlip cell a)) board
+    List.partition (\a -> (willFlip cell a)) board
+
 
 cellSorter : Cell -> Cell -> Order
 cellSorter a b =
     let
-        akey = (toString a.x) ++ (toString a.y)
-        bkey = (toString b.x) ++ (toString b.y)
+        akey =
+            (toString a.x) ++ (toString a.y)
+
+        bkey =
+            (toString b.x) ++ (toString b.y)
     in
-    compare akey bkey
+        compare akey bkey
+
 
 willFlip : Cell -> Cell -> Bool
 willFlip cell potential =
     let
-        x = cell.x
-        y = cell.y
-        xleft = x - 1
-        xright = x + 1
-        yup = y - 1
-        ydown = y + 1
+        x =
+            cell.x
+
+        y =
+            cell.y
+
+        xleft =
+            x - 1
+
+        xright =
+            x + 1
+
+        yup =
+            y - 1
+
+        ydown =
+            y + 1
     in
-    if cell.x == potential.x && cell.y == potential.y
-    then True
-    else if potential.x == xleft && potential.y == y
-    then True
-    else if potential.x == xright && potential.y == y
-    then True
-    else if potential.x == x && potential.y == yup
-    then True
-    else if potential.x == x && potential.y == ydown
-    then True
-    else False
+        if cell.x == potential.x && cell.y == potential.y then
+            True
+        else if potential.x == xleft && potential.y == y then
+            True
+        else if potential.x == xright && potential.y == y then
+            True
+        else if potential.x == x && potential.y == yup then
+            True
+        else if potential.x == x && potential.y == ydown then
+            True
+        else
+            False
+
 
 chunksOfLeft : Int -> List a -> List (List a)
 chunksOfLeft k xs =
-  let len = List.length xs
-  in  if len > k
-      then List.take k xs :: chunksOfLeft k (List.drop k xs)
-      else [xs]
+    let
+        len =
+            List.length xs
+    in
+        if len > k then
+            List.take k xs :: chunksOfLeft k (List.drop k xs)
+        else
+            [ xs ]
+
+
 
 -- SUBSCRIPTIONS
 
@@ -175,43 +222,55 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     case model.currentGame of
-        Just game -> showRunningGame game
-        Nothing -> div[] 
-                    [ button [ onClick NewGame] [text "start game" ]
-                    ]
+        Just game ->
+            showRunningGame game
+
+        Nothing ->
+            div []
+                [ button [ onClick NewGame ] [ text "start game" ]
+                ]
 
 
 showRunningGame : Game -> Html Msg
 showRunningGame game =
     let
-        chunked = chunksOfLeft game.size game.board
+        chunked =
+            chunksOfLeft game.size game.board
     in
-    div []
-        [ text "game on!"
-        , text ( toString game.moves)
-        , text (toString game.gameWon)
-        , drawGame chunked
-        ]
+        div []
+            [ text "game on!"
+            , text (toString game.moves)
+            , text (toString game.gameWon)
+            , drawGame chunked
+            ]
+
 
 
 --decideScreen : Model -> Html Msg
 
+
 drawGame : List (List Cell) -> Html Msg
 drawGame rows =
-    div[] <| List.map drawRow rows
+    div [] <| List.map drawRow rows
+
 
 drawRow : List Cell -> Html Msg
 drawRow cells =
-    div[] <| List.map drawCell cells 
+    div [] <| List.map drawCell cells
+
 
 drawCell : Cell -> Html Msg
 drawCell cell =
     case cell.selected of
-        True -> span[ selectedStyle, onClick (Selected cell) ] [ text (toString cell) ]
-        False -> span[ onClick (Selected cell) ] [ text (toString cell) ]
+        True ->
+            span [ selectedStyle, onClick (Selected cell) ] [ text (toString cell) ]
+
+        False ->
+            span [ onClick (Selected cell) ] [ text (toString cell) ]
+
 
 selectedStyle : Attribute msg
 selectedStyle =
-  style
-    [ ("backgroundColor", "red")
-    ]
+    style
+        [ ( "backgroundColor", "red" )
+        ]
